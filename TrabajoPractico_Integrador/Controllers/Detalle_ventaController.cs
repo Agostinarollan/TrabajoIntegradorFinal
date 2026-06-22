41781761927ccae14f2550c514de4fd715b6d283
Agostinarollan <agostinarollan@gmail.com>
2026-06-22 19:38:07 -0300

    Correciones de controller

--- TrabajoPractico_Integrador/Controllers/Detalle_ventaController.cs
+++ TrabajoPractico_Integrador/Controllers/Detalle_ventaController.cs
@@ -10,23 +10,23 @@ using TrabajoPractico_Integrador.Models;
 
 namespace TrabajoPractico_Integrador.Controllers
 {
-    public class VentasController : Controller
+    public class Detalle_ventaController : Controller
     {
         private readonly AppDbContext _context;
 
-        public VentasController(AppDbContext context)
+        public Detalle_ventaController(AppDbContext context)
         {
             _context = context;
         }
 
-        // GET: Ventas
+        // GET: Detalle_venta
         public async Task<IActionResult> Index()
         {
-            var appDbContext = _context.Ventas.Include(v => v.Cliente).Include(v => v.Usuario);
+            var appDbContext = _context.DetalleVentas.Include(d => d.Producto).Include(d => d.Venta);
             return View(await appDbContext.ToListAsync());
         }
 
-        // GET: Ventas/Details/5
+        // GET: Detalle_venta/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -34,45 +34,45 @@ namespace TrabajoPractico_Integrador.Controllers
                 return NotFound();
             }
 
-            var venta = await _context.Ventas
-                .Include(v => v.Cliente)
-                .Include(v => v.Usuario)
+            var detalle_venta = await _context.DetalleVentas
+                .Include(d => d.Producto)
+                .Include(d => d.Venta)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (venta == null)
+            if (detalle_venta == null)
             {
                 return NotFound();
             }
 
-            return View(venta);
+            return View(detalle_venta);
         }
 
-        // GET: Ventas/Create
+        // GET: Detalle_venta/Create
         public IActionResult Create()
         {
-            ViewData["ClienteID"] = new SelectList(_context.Clientes, "Id", "Nombre");
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "Id", "Nombre");
+            ViewData["ProductoID"] = new SelectList(_context.Productos, "Id", "Nombre");
+            ViewData["VentaID"] = new SelectList(_context.Ventas, "Id", "Id");
             return View();
         }
 
-        // POST: Ventas/Create
+        // POST: Detalle_venta/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,fecha,Total,Estado,ClienteID,UsuarioID")] Venta venta)
+        public async Task<IActionResult> Create([Bind("Id,Cantidad,PrecioUnitario,Subtotal,ProductoID,VentaID")] Detalle_venta detalle_venta)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(venta);
+                _context.Add(detalle_venta);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ClienteID"] = new SelectList(_context.Clientes, "Id", "Nombre", venta.ClienteID);
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "Id", "Nombre", venta.UsuarioID);
-            return View(venta);
+            ViewData["ProductoID"] = new SelectList(_context.Productos, "Id", "Nombre", detalle_venta.ProductoID);
+            ViewData["VentaID"] = new SelectList(_context.Ventas, "Id", "Id", detalle_venta.VentaID);
+            return View(detalle_venta);
         }
 
-        // GET: Ventas/Edit/5
+        // GET: Detalle_venta/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,24 +80,24 @@ namespace TrabajoPractico_Integrador.Controllers
                 return NotFound();
             }
 
-            var venta = await _context.Ventas.FindAsync(id);
-            if (venta == null)
+            var detalle_venta = await _context.DetalleVentas.FindAsync(id);
+            if (detalle_venta == null)
             {
                 return NotFound();
             }
-            ViewData["ClienteID"] = new SelectList(_context.Clientes, "Id", "Nombre", venta.ClienteID);
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "Id", "Nombre", venta.UsuarioID);
-            return View(venta);
+            ViewData["ProductoID"] = new SelectList(_context.Productos, "Id", "Nombre", detalle_venta.ProductoID);
+            ViewData["VentaID"] = new SelectList(_context.Ventas, "Id", "Id", detalle_venta.VentaID);
+            return View(detalle_venta);
         }
 
-        // POST: Ventas/Edit/5
+        // POST: Detalle_venta/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,fecha,Total,Estado,ClienteID,UsuarioID")] Venta venta)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Cantidad,PrecioUnitario,Subtotal,ProductoID,VentaID")] Detalle_venta detalle_venta)
         {
-            if (id != venta.Id)
+            if (id != detalle_venta.Id)
             {
                 return NotFound();
             }
@@ -106,12 +106,12 @@ namespace TrabajoPractico_Integrador.Controllers
             {
                 try
                 {
-                    _context.Update(venta);
+                    _context.Update(detalle_venta);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!VentaExists(venta.Id))
+                    if (!Detalle_ventaExists(detalle_venta.Id))
                     {
                         return NotFound();
                     }
@@ -122,12 +122,12 @@ namespace TrabajoPractico_Integrador.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ClienteID"] = new SelectList(_context.Clientes, "Id", "Nombre", venta.ClienteID);
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "Id", "Nombre", venta.UsuarioID);
-            return View(venta);
+            ViewData["ProductoID"] = new SelectList(_context.Productos, "Id", "Nombre", detalle_venta.ProductoID);
+            ViewData["VentaID"] = new SelectList(_context.Ventas, "Id", "Id", detalle_venta.VentaID);
+            return View(detalle_venta);
         }
 
-        // GET: Ventas/Delete/5
+        // GET: Detalle_venta/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -135,36 +135,36 @@ namespace TrabajoPractico_Integrador.Controllers
                 return NotFound();
             }
 
-            var venta = await _context.Ventas
-                .Include(v => v.Cliente)
-                .Include(v => v.Usuario)
+            var detalle_venta = await _context.DetalleVentas
+                .Include(d => d.Producto)
+                .Include(d => d.Venta)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (venta == null)
+            if (detalle_venta == null)
             {
                 return NotFound();
             }
 
-            return View(venta);
+            return View(detalle_venta);
         }
 
-        // POST: Ventas/Delete/5
+        // POST: Detalle_venta/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var venta = await _context.Ventas.FindAsync(id);
-            if (venta != null)
+            var detalle_venta = await _context.DetalleVentas.FindAsync(id);
+            if (detalle_venta != null)
             {
-                _context.Ventas.Remove(venta);
+                _context.DetalleVentas.Remove(detalle_venta);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool VentaExists(int id)
+        private bool Detalle_ventaExists(int id)
         {
-            return _context.Ventas.Any(e => e.Id == id);
+            return _context.DetalleVentas.Any(e => e.Id == id);
         }
     }
 }
